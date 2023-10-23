1ef7d3a5c7fe713300728da9e6f69e417fddf667
andres <a.dossantos@alumnos.upm.es>
2023-10-23 10:40:03 +0200

    POM

--- src/test/java/es/upm/grise/profundizacion/control_1/LibraryTest.java
+++ src/test/java/es/upm/grise/profundizacion/control_1/LibraryTest.java
@@ -1,6 +1,13 @@
 package es.upm.grise.profundizacion.control_1;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class LibraryTest {
+	
+	static Library lib;
 	
 	@Test
 	public void test() {
